2b17b4d27c8eb81e202c99c56829595686ba5258
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-12 10:55:09 +0300

    ft:imported error html template

---
+++ app/main/errors.py
@@ -0,0 +1,8 @@
+from xml.dom.pulldom import ErrorHandler
+from flask import render_template
+from . import main
+
+@main.errorhandler(404)
+def not_found(error):
+    return render_template('error.html', error=error),404
+    

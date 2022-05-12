b50e2923e5810d532b3edc7141eb954c4b73ce14
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-12 23:45:54 +0300

    ft:added fields for remaining forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,28 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms  import StringField,PasswordField,SubmitField, ValidationError,BooleanField
-from wtforms.validators import Required, Length,Email
+from wtforms.validators import input_required, Length,Email,EqualTo
+from ..models import User
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField
-    username = StringField
-    password = PasswordField
-    password_confirm = PasswordField
-    submit = SubmitField
+    email = StringField('Your email address', validators=[input_required(),Email()])
+    
+    username = StringField('Enter your username', validators=[input_required()])
+    password = PasswordField('Password',validators = [input_required(),EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Password',validators = [input_required])
+    submit = SubmitField('Sign Up')
     
     
 class LoginForm(FlaskForm):
-    email = StringField
-    password = PasswordField
-    remember = BooleanField
-    submit = SubmitField
+    email = StringField('Your email address', validators = [input_required(),Email()])
+    password = PasswordField('Password',validators = [input_required()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
     
 class ResetPasswordForm(FlaskForm):
-    email = StringField
-    submit = SubmitField
+    email = StringField('Email',validators = [input_required(), Email()])
+    submit = SubmitField('Reset Password')
     
     
 class NewPasswordForm(FlaskForm):
-    password = PasswordField
-    password_repeat = PasswordField
-    submit = SubmitField
+    password = PasswordField('Password',validators = [input_required()])
+    password_repeat = PasswordField('RepeatPassword',validators = [input_required(),EqualTo('password')])
+    submit = SubmitField('Change Password')

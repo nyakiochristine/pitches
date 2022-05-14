b2a05d05c83af0380e8a5796261e57f93cedf341
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-15 02:07:58 +0300

    ft:created form content

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,30 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms  import StringField,PasswordField,SubmitField, ValidationError,BooleanField
-from wtforms.validators import input_required, Length,Email,EqualTo
+from wtforms.validators import  Length,Email,EqualTo
 from ..models import User
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Your email address', validators=[input_required(),Email()])
+    email = StringField('Your email address', validators=[Email()])
     
-    username = StringField('Enter your username', validators=[input_required()])
-    password = PasswordField('Password',validators = [input_required(),EqualTo('password_confirm',message = 'Passwords must match')])
-    password_confirm = PasswordField('Confirm Password',validators = [input_required])
+    username = StringField('Enter your username', validators=[])
+    password = PasswordField('Password',validators = [EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Password',validators = [])
     submit = SubmitField('Sign Up')
     
     
 class LoginForm(FlaskForm):
-    email = StringField('Your email address', validators = [input_required(),Email()])
-    password = PasswordField('Password',validators = [input_required()])
+    email = StringField('Your email address', validators = [Email()])
+    password = PasswordField('Password',validators = [])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Log In')
     
 class ResetPasswordForm(FlaskForm):
-    email = StringField('Email',validators = [input_required(), Email()])
+    email = StringField('Email',validators = [ Email()])
     submit = SubmitField('Reset Password')
     
     
 class NewPasswordForm(FlaskForm):
-    password = PasswordField('Password',validators = [input_required()])
-    password_repeat = PasswordField('RepeatPassword',validators = [input_required(),EqualTo('password')])
+    password = PasswordField('Password',validators = [])
+    password_repeat = PasswordField('RepeatPassword',validators = [EqualTo('password')])
     submit = SubmitField('Change Password')

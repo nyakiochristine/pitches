f2e0717e7e4774e2e544ba993ab386b8053baf4d
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-15 02:06:31 +0300

    ft:added manager shell decorator

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
-from app import create_app
+from app import create_app,db
 from flask import Flask
 from flask_script import Manager,Server
+from app.models import User,Pitch, Comment
 
 
 #instance for creating the app
@@ -8,6 +9,10 @@ app = create_app('production')
 manager = Manager(app)
 
 manager.add_command('server', Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Pitch = Pitch)
 
 if __name__=='__main__':
     manager.run()
